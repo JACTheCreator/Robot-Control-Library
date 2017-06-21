001aef676cbce856769873abcab0f13d2545bcb7
Jermaine Coates <jermainecoates@outlook.com>
2017-06-21 12:46:26 -0500

    Fixed Syntax Error

--- examples/MotorContol/One_DC_Motor_Control.ino
+++ examples/MotorContol/One_DC_Motor_Control.ino
@@ -1,7 +1,7 @@
 #include <MotorControl.h>
 
 //Pins the Motor is connected to -> 8 and 9
-MotorControl oneMotor(8, 9); // also MotorControl oneMotor = MotorControl(8, 9)
+DC_Motor oneMotor(8, 9); // also MotorControl oneMotor = MotorControl(8, 9)
 
 void setup()
 {
